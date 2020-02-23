95a8cfcdfdec2332c64937fa0b6aadca2c5e11af
correlator <scotthoch86@gmail.com>
2020-02-22 20:22:47 -0800

    only render json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
-  resource :shop, only: [:show]
+  resource :shop, only: [:show], defaults: { format: :json }
 
   mount ShopifyApp::Engine, at: '/'
 end
